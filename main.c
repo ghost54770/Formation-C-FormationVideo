05d50d79c47071fc5ec215e285f146b18e469e59
ghost54770 <ghost54.lol@hotmail.fr>
2021-04-10 17:21:33 +0200

    avancement

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #include "fonction.h"
 
 
+
+
 void main (void){
     MessageDate();
 
+
+    register int nombre1 = 0;
+    register int nombre2 = 0;
+
+
+    for( nombre1 = 1;  nombre1 < 5000 ; nombre1++){
+        nombre2 = nombre1;
+        nombre1 += nombre2;
+    }
+
+
     /*
     %d : nombre entier (int)
-    %f : nombre flottant  (float)
+    %f : nombre flottant  (float)    %.2f  = 2chiffre apres la virgule
     %c : caractere (char)
     %s : chaine de caractere 
     */
-    printf("%d    %f     %c      %s",5,8.4,'C',"salut");
+    printf("%d    %.2f     %c      %s",5,3.6,'C',"salut");
 
 }
